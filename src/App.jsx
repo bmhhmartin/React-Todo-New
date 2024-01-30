b678a6e3b456cfddbd7dea1dc5c38fc6a0a2e9f1
Md H H Martin <35525254+bmhhmartin@users.noreply.github.com>
2024-01-30 18:54:52 +0600

    Markup added

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import TaskBoard from "./components/TaskComponents/TaskBoard";
+
+const App =()=>{
+  return (
+    <>
+      <Header></Header>
+      <Hero></Hero>
+      <TaskBoard></TaskBoard>
+      <Footer></Footer>
+    </>
+  )
+}
+export default App;
