94f997ec2b31aff7d47debbb306136bb5a533830
Md H H Martin <35525254+bmhhmartin@users.noreply.github.com>
2024-01-31 19:17:32 +0600

    Add task functionality added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import Hero from "./components/Hero";
 import TaskBoard from "./components/TaskComponents/TaskBoard";
 
 const App =()=>{
   return (
     <>
       <Header></Header>
-      <Hero></Hero>
       <TaskBoard></TaskBoard>
       <Footer></Footer>
     </>
